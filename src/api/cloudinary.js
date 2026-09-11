f30eaf5cf3650035598ac5d2ec261862c7f1210f
aartikumarsingh <aartikumarsingh555@gmail.com>
2026-09-11 21:23:38 +0530

    Fix syntax error in cloudinary.js

--- src/api/cloudinary.js
+++ src/api/cloudinary.js
@@ -12,8 +12,8 @@ export const IMAGES = [
     "size": "24 x 30 inches",
     "description": "A close-up hand painted acrylic of Lord Krishna lost in his flute's melody, eyes closed in blissful devotion, adorned with roses, a peacock feather and a rudraksha mala against a warm, glowing backdrop. ✅ 100% Hand painted Acrylic on Canvas. ✅ Rich detailing — floral crown, jewellery & flowing robes. ✅ Perfect for pooja rooms, living rooms & festive gifting. Let the music of Krishna fill your space. 🪈🙏.",
     "medium": "Acrylic on Canvas",
-    "isSold": false
-                "isSold: false......
+    "isSold": false,
+
     },
          {
     "public_id": "🦚 Divine Melody – Lord Krishna Acrylic Painting 🦚",
